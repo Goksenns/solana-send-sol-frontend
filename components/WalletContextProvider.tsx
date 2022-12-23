b887240ef3b27e953fa59a8bae2c413ed047c96a
Gökşen Ayar <goksenns@gmail.com>
2022-12-23 22:19:09 +0300

    send sol

---
+++ components/WalletContextProvider.tsx
@@ -0,0 +1,22 @@
+import { FC, ReactNode } from "react";
+import * as web3 from "@solana/web3.js";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import "@solana/wallet-adapter-react-ui/sytles.css";
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const wallet = [new PhantomWalletAdapter()];
+
+  const endpoint = web3.clusterApiUrl("devnet");
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallet}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
